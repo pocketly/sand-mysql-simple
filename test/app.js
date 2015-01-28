301dd5adb80d8862f75d07152dc4f60c4e52e303
Adam Jaso <ajaso@sazze.com>
2015-01-27 16:23:32 -0800

    added shims for on duplicate key update

--- test/app.js
+++ test/app.js
@@ -14,102 +14,153 @@ var sand = require('sand')({appPath: __dirname+'/..'})
 
 var mocks = require('./mocks');
 
-before(function(done) {
-  sand.start(done);
+describe('mysqlSimple', function() {
+
+  before(function (done) {
+    sand.start(done);
+  });
+
+  describe('mysqlSimple.Model', function () {
+
+    var mock;
+    beforeEach(function () {
+      if (mock) {
+        mock.restore();
+        mock = null;
+      }
+    });
+
+    it('should return a knex query builder', function () {
+
+      var builder = models.User();
+      builder.select().toSQL().sql.should.match(/`user`/)
+
+    });
+
+    it('should select a single row', function (done) {
+
+      var select = mocks.exampleSelect;
+
+      mock = sinon.mock(sand.mysql);
+      mocks.mysqlQuery(mock, select.sql, select.bindings, [null, [mocks.exampleResultRow]]);
+
+      models.User.selectRow(mocks.exampleWhere, function (err, row) {
+        mock.verify();
+        mocks.exampleResultRow.should.eql(row);
+        done();
+      });
+
+    });
+
+    it('should insert a single row', function (done) {
+
+      var insert = mocks.exampleInsert;
+
+      mock = sinon.mock(sand.mysql);
+      mocks.mysqlQuery(mock, insert.sql, insert.bindings, [null, mocks.exampleInsertResult]);
+
+      models.User.insert(mocks.exampleResultRow, function (err, result) {
+        mock.verify();
+        mocks.exampleInsertResult.should.eql(result);
+        done();
+      });
+
+    });
+
+    it('should update a single row', function (done) {
+
+      var update = mocks.exampleUpdate;
+
+      mock = sinon.mock(sand.mysql);
+      mocks.mysqlQuery(mock, update.sql, update.bindings, [null, mocks.exampleUpdateResult]);
+
+      models.User.update(mocks.exampleResultRow, mocks.exampleWhere, function (err, result) {
+        mock.verify();
+        mocks.exampleUpdateResult.should.eql(result);
+        done();
+      });
+
+    });
+
+    it('should delete a single row', function (done) {
+
+      var del = mocks.exampleDelete;
+
+      mock = sinon.mock(sand.mysql);
+      mocks.mysqlQuery(mock, del.sql, del.bindings, [null, mocks.exampleDeleteResult]);
+
+      models.User.delete(mocks.exampleWhere, function (err, result) {
+        mock.verify();
+        mocks.exampleDeleteResult.should.eql(result);
+        done();
+      });
+
+    });
+
+    it('should remove invalid values from insert', function (done) {
+
+      var insert = models.User().insert({id: 1}).toSQL();
+
+      mock = sinon.mock(sand.mysql);
+      mocks.mysqlQuery(mock, insert.sql, insert.bindings, [null, mocks.exampleInsertResult]);
+
+      models.User.insert({id: 1, name: undefined}, function (err, result) {
+        mock.verify();
+
+        done();
+
+      });
+
+    });
+
+  });
+
+  describe('mysqlSimple.Util', function() {
+
+    describe('buildOnDuplicateKeyIgnore', function() {
+
+      it('should build on duplicate key ignore', function() {
+
+        ' ON DUPLICATE KEY UPDATE `column` = `column`'.should.be.eql(simple.Util.buildOnDuplicateKeyIgnore('column'));
+
+      });
+
+    });
+
+    describe('buildOnDuplicateKeyUpdate', function() {
+
+      var oneCol = ' ON DUPLICATE KEY UPDATE `column1` = VALUES(`column1`)';
+      var twoCols = ' ON DUPLICATE KEY UPDATE `column1` = VALUES(`column1`), `column2` = VALUES(`column2`)';
+
+      var tests = [
+        {
+          columns: 'column1',
+          expected: oneCol
+        },
+        {
+          columns: ['column1'],
+          expected: oneCol
+        },
+        {
+          columns: {'column1': 1, column2: 2},
+          expected: twoCols
+        },
+        {
+          columns: ['column1', 'column2'],
+          expected: twoCols
+        }
+      ];
+
+      _.each(tests, function(test) {
+        it('should build on duplicate key update for: ' + JSON.stringify(test.columns), function() {
+
+          test.expected.should.be.eql(simple.Util.buildOnDuplicateKeyUpdate(test.columns));
+
+        });
+      });
+
+    });
+
+  });
+
 });
-
-describe('mysqlSimple.Model', function () {
-
-  var mock;
-  beforeEach(function() {
-    if (mock) {
-      mock.restore();
-      mock = null;
-    }
-  });
-
-  it('should return a knex query builder', function() {
-
-    var builder = models.User();
-    builder.select().toSQL().sql.should.match(/`user`/)
-
-  });
-
-  it('should select a single row', function(done) {
-
-    var select = mocks.exampleSelect;
-
-    mock = sinon.mock(sand.mysql);
-    mocks.mysqlQuery(mock, select.sql, select.bindings, [null, [mocks.exampleResultRow]]);
-
-    models.User.selectRow(mocks.exampleWhere, function(err, row) {
-      mock.verify();
-      mocks.exampleResultRow.should.eql(row);
-      done();
-    });
-
-  });
-
-  it('should insert a single row', function(done) {
-
-    var insert = mocks.exampleInsert;
-
-    mock = sinon.mock(sand.mysql);
-    mocks.mysqlQuery(mock, insert.sql, insert.bindings, [null, mocks.exampleInsertResult]);
-
-    models.User.insert(mocks.exampleResultRow, function(err, result) {
-      mock.verify();
-      mocks.exampleInsertResult.should.eql(result);
-      done();
-    });
-
-  });
-
-  it('should update a single row', function(done) {
-
-    var update = mocks.exampleUpdate;
-
-    mock = sinon.mock(sand.mysql);
-    mocks.mysqlQuery(mock, update.sql, update.bindings, [null, mocks.exampleUpdateResult]);
-
-    models.User.update(mocks.exampleResultRow, mocks.exampleWhere, function(err, result) {
-      mock.verify();
-      mocks.exampleUpdateResult.should.eql(result);
-      done();
-    });
-
-  });
-
-  it('should delete a single row', function(done) {
-
-    var del = mocks.exampleDelete;
-
-    mock = sinon.mock(sand.mysql);
-    mocks.mysqlQuery(mock, del.sql, del.bindings, [null, mocks.exampleDeleteResult]);
-
-    models.User.delete(mocks.exampleWhere, function(err, result) {
-      mock.verify();
-      mocks.exampleDeleteResult.should.eql(result);
-      done();
-    });
-
-  });
-
-  it('should remove invalid values from insert', function(done) {
-
-    var insert = models.User().insert({id: 1}).toSQL();
-
-    mock = sinon.mock(sand.mysql);
-    mocks.mysqlQuery(mock, insert.sql, insert.bindings, [null, mocks.exampleInsertResult]);
-
-    models.User.insert({id: 1, name: undefined}, function(err, result) {
-      mock.verify();
-
-      done();
-
-    });
-
-  });
-
-});
-
